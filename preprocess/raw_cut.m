ca8b39685faf0ed5ef8dfe2fc01b46f21106ccf1
ZHUZ <51739967+zhuz-bit@users.noreply.github.com>
2024-03-31 15:08:32 +0800

    Update raw_cut.m

--- preprocess/raw_cut.m
+++ preprocess/raw_cut.m
@@ -3,10 +3,7 @@
 clc
 % https://blog.csdn.net/hahahahhahha/article/details/103850662
 % https://blog.csdn.net/hahahahhahha/article/details/118157344
-%图像基本信息：
-%512行，640列，像素深度16bit,帧数不确定。
 
-%step1：获得图像的帧数L
 numCols=2048;
 numRows=2448;
 numPixelsPerFrame=numCols*numRows;
@@ -14,17 +11,16 @@ pathname_in="D:\video-1.13\savedvideos";
 filename_in="9.raw";
 pathfilename_in=fullfile(pathname_in,filename_in);
 fid=fopen(pathfilename_in,'rb');
-fseek(fid,0,'eof');
-%这里不要关闭句柄！！
+
 numBytesTotalFrame=ftell(fid);
 numFrames=floor(numBytesTotalFrame/(2*numPixelsPerFrame));
 
 if numFrames<1
-    errordlg('帧数小于1，文件大小不够一帧图像');
+    errordlg('zx');
     return
 end
 
-%step2：拆分一帧一帧的读图像
+
 xx=floor(log10(numFrames))+2;
 infor="202106221_raw";
 pathname_out="D:\out\savedvideos\9";
@@ -32,7 +28,7 @@ pathname_out="D:\out\savedvideos\9";
 
 % A=zeros(NumPixelsPerFrame,numFrames);
 
-h=waitbar(0,'正在计算中');
+h=waitbar(0,'xx');
 for  ii=1:numFrames
     fseek(fid,(ii-1)*(2*numPixelsPerFrame),'bof');
     temp=fread(fid,numPixelsPerFrame,'uint16');
@@ -45,12 +41,12 @@ for  ii=1:numFrames
     pathfilename_out=fullfile(pathname_out,filename_out);
     fid_1=fopen(pathfilename_out,"wb");
     fwrite(fid_1,temp,'uint16');
-    waitbar(ii/numFrames,h,{'正在修改文件名>>>>>>>>>';....
+    waitbar(ii/numFrames,h,{'changing>>>>>>>>>';....
         ['共',num2str(numFrames),'帧 ','已处理',num2str(ii),'帧']});
     fclose(fid_1);
     
 end
-fclose(fid);%在最后，载进行关闭句柄！！
+fclose(fid);
 close(h);
 
 disp("ok~");
